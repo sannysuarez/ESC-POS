4e1aea18dd38efa7be5355a081e7282ab371d435
sanny suarez <sannysuarez4@gmail.com>
2026-03-22 16:22:39 +0100

    remove unused file

--- RAASU/v1.0.1/launcher.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Force Kivy to use ANGLE (DirectX instead of OpenGL)
-os.environ["KIVY_GL_BACKEND"] = "angle_sdl2"
-os.environ["KIVY_NO_ARGS"] = "1"
-
-# Run the main program
-import main
